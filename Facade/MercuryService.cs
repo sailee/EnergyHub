97b1fea3874a08f4e31746c030dbfa872a20fe25
Sean Helvey <smh436@nyu.edu>
2012-03-10 15:17:20 -0500

    MercuryService ObtainMobileAuthToken() is complete

--- Facade/MercuryService.cs
+++ Facade/MercuryService.cs
@@ -10,96 +10,58 @@ using System.IO;
 using Facade;
 using System.Windows.Forms;
 
-
 namespace Facade
 {
+    //Service for interaction with the Mercury server
     public class MercuryService
     {
+        //HttpWebRequest to Mercury posting credentials to retrieve token
         public String ObtainMobileAuthToken()
         {
-
             try
             {
+                //Web Request
                 HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("https://mercury-beta.energyhub.net/filtrete/rest/public/mobileAuth");
+
+                //Credentials, Method, ContentType
                 myReq.Credentials = new NetworkCredential("sean.helvey@gmail.com", "Br@infood11");
                 myReq.Method = @"POST";
                 myReq.ContentType = @"application/x-www-form-urlencoded; charset=utf-8";
+                
+                //Data, Encoding, Size
                 string Body = "j_username=sean.helvey@gmail.com&j_password=Br@infood11";
                 byte[] data = Encoding.UTF8.GetBytes(Body);
                 myReq.ContentLength = data.Length;
-                using(Stream stream = myReq.GetRequestStream())
-                    stream.Write(data, 0, data.Length);
-                using(Stream responseStream = myReq.GetResponse().GetResponseStream())
-                using(StreamReader reader = new StreamReader(responseStream, Encoding.UTF8))
-                {
-                    string result = reader.ReadToEnd();
-                }
 
-                HttpWebResponse response = (HttpWebResponse)myReq.GetResponse();
-                Stream resStream = response.GetResponseStream();
+                //Request Stream
+                Stream stream = myReq.GetRequestStream();
+                stream.Write(data, 0, data.Length);
 
-                // used to build entire input
-                StringBuilder sb = new StringBuilder();
-
-                // used on each read operation
-                byte[] buf = new byte[8192];
-
-                string tempString = null;
-                int count = 0;
-
-                do
-                {
-                    // fill the buffer with data
-                    count = resStream.Read(buf, 0, buf.Length);
-
-                    // make sure we read some data
-                    if (count != 0)
-                    {
-                        // translate from bytes to ASCII text
-                        tempString = Encoding.ASCII.GetString(buf, 0, count);
-
-                        // continue building the string
-                        sb.Append(tempString);
-                    }
-                }
-                while (count > 0);
-
-                //DeSerialize(sb.ToString());
-                string authToken = JsonMercuryDeserialize(sb.ToString());
-                return authToken;
+                //Response Stream
+                Stream responseStream = myReq.GetResponse().GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8); 
+            
+                //Stream to String
+                string result = reader.ReadToEnd();
+                reader.Close();
+                //String to Object
+                JObject jsonObj = JObject.Parse(result);
+                //Object to Token
+                string mobileAuthToken = (string)jsonObj["value"];
+          
+                //Return Token
+                return mobileAuthToken;
             }
 
-            catch (WebException Exception1)
+            catch (WebException)
             {
-                throw new WebException("Could not obtain auth token from server.", Exception1);
+                throw new WebException("WebException");
             }
 
-            catch (Exception Exception2)
+            catch (Exception)
             {
-                throw new Exception("Could not obtain auth token from server.", Exception2);
+                throw new Exception("Exception");
             }
-        }
-
-        private string JsonMercuryDeserialize(string json)
-        {
-            //JObject jsonObj = JObject.Parse(json);
-
-            //JArray networks = (JArray)jsonObj["networks"];
-
-            //List<JsonNetwork> nets = new List<JsonNetwork>();
-
-            //foreach (JToken token in networks)
-            //{
-            //    String ssid = (String)token[0];
-            //    String bssid = (String)token[1];
-            //    int secMode = (int)token[2];
-            //    int Channel = (int)token[3];
-            //    int rssi = (int)token[4];
-
-            //    JsonNetwork net = new JsonNetwork(ssid, bssid, secMode, Channel, rssi);
-            //    nets.Add(net);
-            //}
-            return json;
-        }
+        }       
     }
 }
--- TstatMgmtGUI/Program.cs
+++ TstatMgmtGUI/Program.cs
@@ -16,8 +16,8 @@ namespace TstatMgmtGUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Test());
-            Application.Run(new ServiceDiscovery());
-            //Application.Run(new SplashScreen());
+            //Application.Run(new ServiceDiscovery());
+            Application.Run(new SplashScreen());
         }
     }
 }
